a3878e21a825e9338b57fdf9c8de62118c2aa536
Patrick Kabwe <patrickckabwe@gmail.com>
2024-07-09 05:59:54 +0200

    fix: failing tests

--- credentials_test.go
+++ credentials_test.go
@@ -1,35 +1,34 @@
 package fcm
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCredentials_Validate(t *testing.T) {
 	testCases := []struct {
 		name        string
 		credentials *Credentials
+		expectsErr  bool
 	}{
 		{
-			name: "project_id is required", credentials: &Credentials{},
+			name:        "project_id is required",
+			credentials: &Credentials{},
+			expectsErr:  true,
 		},
 		{
-			name: "private_key is required",
-			credentials: &Credentials{
-				ProjectID: "project_id",
-			},
+			name:        "private_key is required",
+			credentials: &Credentials{ProjectID: "project_id"},
+			expectsErr:  true,
 		},
 		{
-			name: "client_email is required",
-			credentials: &Credentials{
-				ProjectID:  "project_id",
-				PrivateKey: "private",
-			},
+			name:        "client_email is required",
+			credentials: &Credentials{ProjectID: "project_id", PrivateKey: "private"},
+			expectsErr:  true,
 		},
 		{
-			name: "client_id is required",
-			credentials: &Credentials{
-				ProjectID:   "project_id",
-				PrivateKey:  "private",
-				ClientEmail: "email",
-			},
+			name:        "client_id is required",
+			credentials: &Credentials{ProjectID: "project_id", PrivateKey: "private", ClientEmail: "email"},
+			expectsErr:  true,
 		},
 		{
 			name: "auth_uri is required",
@@ -39,6 +38,7 @@ func TestCredentials_Validate(t *testing.T) {
 				ClientEmail: "email",
 				ClientID:    "client_id",
 			},
+			expectsErr: true,
 		},
 		{
 			name: "token_uri is required",
@@ -49,6 +49,7 @@ func TestCredentials_Validate(t *testing.T) {
 				ClientID:    "client_id",
 				AuthURI:     "auth_uri",
 			},
+			expectsErr: true,
 		},
 		{
 			name: "auth_provider_x509_cert_url is required",
@@ -60,6 +61,7 @@ func TestCredentials_Validate(t *testing.T) {
 				AuthURI:     "auth_uri",
 				TokenURI:    "token_uri",
 			},
+			expectsErr: true,
 		},
 		{
 			name: "client_x509_cert_url is required",
@@ -72,6 +74,7 @@ func TestCredentials_Validate(t *testing.T) {
 				TokenURI:                "token_uri",
 				AuthProviderX509CertURL: "auth_provider_x509_cert_url",
 			},
+			expectsErr: true,
 		},
 		{
 			name: "valid credentials",
@@ -85,14 +88,17 @@ func TestCredentials_Validate(t *testing.T) {
 				AuthProviderX509CertURL: "auth_provider_x509_cert_url",
 				ClientX509CertURL:       "client_x509_cert_url",
 			},
+			expectsErr: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			err := tc.credentials.Validate()
-			if err == nil {
+			if tc.expectsErr && err == nil {
 				t.Errorf("expected error, got nil")
+			} else if !tc.expectsErr && err != nil {
+				t.Errorf("expected no error, got %v", err)
 			}
 		})
 	}
